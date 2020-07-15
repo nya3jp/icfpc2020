465448355a3dd7d741d6ba304e1525802ba594a5
Masaya Suzuki <masayasuzuki@google.com>
2020-07-14 21:28:17 -0700

    examples: Update the code to match starterkit-bazel
    The code is clang-formatted.

--- examples/app.cc
+++ examples/app.cc
@@ -1,15 +1,23 @@
+#include <cpr/cpr.h>
+
 #include <iostream>
 #include <string>
 
-#include <cpr/cpr.h>
-
 int main(int argc, char* argv[]) {
-    const std::string server_url{argv[1]};
-    const std::string player_key{argv[2]};
+  const std::string server_url{argv[1]};
+  const std::string player_key{argv[2]};
 
-    std::cout << "ServerUrl: " << server_url << "; PlayerKey: " << player_key << std::endl;
+  std::cout << "ServerUrl: " << server_url << "; PlayerKey: " << player_key
+            << std::endl;
 
-    auto res = cpr::Get(cpr::Url{argv[1]}, cpr::Parameters{{"playerKey", argv[2]}});
-    std::cout << res.status_line << std::endl;
-    return 0;
+  auto res = cpr::Post(cpr::Url{argv[1]}, cpr::Body{argv[2]},
+                       cpr::Header{{"Content-Type", "text/plain"}});
+  if (res.status_code != 200) {
+    std::cout << "Unexpected server response:\nHTTP code: " << res.status_code
+              << "\nResponse body: " << res.text << std::endl;
+    return 2;
+  }
+
+  std::cout << "Server response: " << res.text << std::endl;
+  return 0;
 }
