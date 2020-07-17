f47ceb39fd7e3543ff483c5201dced82adba44df
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2020-07-18 01:51:09 +0900

    Implementing parser

---
+++ tanakh/expr_parser/src/main.rs
@@ -0,0 +1,220 @@
+#[derive(Debug, Clone)]
+enum Literal {
+    Unknown(i64),
+    Var(String),
+    Int(i64),
+}
+
+impl Literal {
+    fn new(s: &str) -> Literal {
+        if s.chars().next().unwrap() == ':' {
+            Literal::Unknown(s[1..].parse().unwrap())
+        } else if s.chars().all(|c| c.is_ascii_digit()) {
+            Literal::Int(s.parse().unwrap())
+        } else {
+            Literal::Var(s.to_string())
+        }
+    }
+
+    fn print(&self) -> String {
+        match self {
+            Literal::Int(n) => format!("{}", n),
+            Literal::Unknown(n) => format!(":{}", n),
+            Literal::Var(s) => format!("{}", s),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+enum Expr {
+    App(Box<Expr>, Box<Expr>),
+    Lit(Literal),
+}
+
+#[derive(Debug)]
+enum Expr2 {
+    App(Box<Expr2>, Vec<Box<Expr2>>),
+    Lit(Literal),
+}
+
+impl Expr {
+    fn refine(&self) -> Expr2 {
+        match self {
+            Expr::App(f, x) => {
+                let mut args = vec![Box::new(x.refine())];
+                let mut f = f.clone();
+                loop {
+                    match *f {
+                        Expr::App(g, y) => {
+                            f = g.clone();
+                            args.push(Box::new(y.refine()));
+                        }
+                        Expr::Lit(s) => {
+                            args.reverse();
+                            return Expr2::App(Box::new(Expr2::Lit(s)), args);
+                        }
+                    }
+                }
+            }
+            Expr::Lit(s) => Expr2::Lit(s.clone()),
+        }
+    }
+}
+
+impl Expr2 {
+    fn print(&self) -> String {
+        match self {
+            Expr2::App(f, args) => {
+                let mut s = format!("({}", f.print());
+                for a in args {
+                    s += &format!(" {}", a.print());
+                }
+                s += ")";
+                s
+            }
+            Expr2::Lit(s) => s.print(),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Value {
+    Int(i64),
+    Nil,
+    Cons(Box<Value>, Box<Value>),
+}
+
+impl Value {
+    fn modulate(&self, v: &mut Vec<bool>) {
+        match self {
+            &Value::Int(n) => {
+                let n = if n >= 0 {
+                    v.push(false);
+                    v.push(true);
+                    n as u64
+                } else {
+                    v.push(false);
+                    v.push(true);
+                    n.abs() as u64
+                };
+
+                let keta = 64 - n.leading_zeros();
+                let t = (keta + 3) / 4;
+
+                for _ in 0..t {
+                    v.push(true);
+                }
+                v.push(false);
+
+                for i in (0..4 * t).rev() {
+                    v.push((n >> i) & 1 == 1);
+                }
+            }
+            Value::Nil => {
+                v.push(false);
+                v.push(false);
+            }
+            Value::Cons(hd, tl) => {
+                v.push(true);
+                v.push(true);
+                hd.modulate(v);
+                tl.modulate(v);
+            }
+        }
+    }
+
+    fn demodulate(it: &mut impl Iterator<Item = bool>) -> Option<Value> {
+        let t0 = it.next()?;
+        let t1 = it.next()?;
+
+        Some(match (t0, t1) {
+            (false, false) => Value::Nil,
+            (true, true) => {
+                let x = Self::demodulate(it)?;
+                let y = Self::demodulate(it)?;
+                Value::Cons(Box::new(x), Box::new(y))
+            }
+            _ => {
+                let mut t = 0;
+                while it.next()? {
+                    t += 1;
+                }
+                let mut v = 0;
+                for i in (0..4 * t).rev() {
+                    v |= (if it.next()? { 1 } else { 0 }) << i;
+                }
+                Value::Int(v)
+            }
+        })
+    }
+
+    fn print(&self) -> String {
+        match self {
+            &Value::Int(n) => format!("{}", n),
+            Value::Nil => "nil".to_string(),
+            Value::Cons(hd, tl) => format!("({}, {})", hd.print(), tl.print()),
+        }
+    }
+}
+
+fn modulate(v: &Value) -> Vec<bool> {
+    let mut ret = vec![];
+    v.modulate(&mut ret);
+    ret
+}
+
+fn demodulate(v: &[bool]) -> Option<Value> {
+    let mut it = v.iter().cloned();
+    let ret = Value::demodulate(&mut it)?;
+    assert!(it.next().is_none());
+    Some(ret)
+}
+
+#[test]
+fn test_mod() {
+    assert_eq!(modulate(&Value::Nil), vec![false, false]);
+}
+
+#[test]
+fn test_demod() {
+    let t = "1101100001111101100010110110001100110110010000";
+    let v = t.chars().map(|c| c == '1').collect::<Vec<_>>();
+    let v = demodulate(&v).unwrap();
+    // eprintln!("{}", demodulate(&v).unwrap().print());
+    // assert_eq!();
+
+    let u = modulate(&v)
+        .into_iter()
+        .map(|b| if b { '1' } else { '0' })
+        .collect::<String>();
+    assert_eq!(t, u);
+}
+
+fn parse<'a>(it: &mut impl Iterator<Item = &'a str>) -> Expr {
+    let s = it.next().unwrap();
+    if s == "ap" {
+        let f = parse(it);
+        let x = parse(it);
+        Expr::App(Box::new(f), Box::new(x))
+    } else {
+        Expr::Lit(Literal::new(s))
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let t = "110110000111011111100001001111110101000000";
+    let t = "110110000111011111100001001111110100110000";
+
+    let v = t.chars().map(|c| c == '1').collect::<Vec<_>>();
+    let v = demodulate(&v).unwrap();
+    println!("{}", &v.print());
+
+    // let mut s = String::new();
+    // std::io::stdin().read_line(&mut s)?;
+    // let mut it = s.split_whitespace();
+    // let e = parse(&mut it);
+    // let e = e.refine();
+    // println!("{}", e.print());
+
+    Ok(())
+}
