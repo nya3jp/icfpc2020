748b0d72b32e86ddad02d8e1ac8cbda0ff5d8a93
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2020-07-18 11:18:09 +0900

    Implement evaluator

--- tanakh/expr_parser/src/main.rs
+++ tanakh/expr_parser/src/main.rs
@@ -1,12 +1,13 @@
 #![allow(dead_code)]
+
+use std::collections::BTreeMap;
+
 #[derive(Debug, Clone)]
 enum Literal {
     Var(String),
     Int(i64),
+    Canvas(Vec<(i64, i64)>),
 }
-
-// 'add', 'div', 'mul', 'neg',
-// 'eq', 'lt',
 
 impl Literal {
     fn new(s: &str) -> Literal {
@@ -21,6 +22,7 @@ impl Literal {
         match self {
             Literal::Int(n) => format!("{}", n),
             Literal::Var(s) => format!("{}", s),
+            Literal::Canvas(ps) => format!("<canvas:{:?}>", ps),
         }
     }
 }
@@ -30,6 +32,7 @@ enum Expr {
     Lam(String, Box<Expr>),
     App(Box<Expr>, Box<Expr>),
     Lit(Literal),
+    Builtin(String, usize, Vec<Expr>),
 }
 
 #[derive(Debug, Clone)]
@@ -49,6 +52,10 @@ impl Expr {
         Expr::Lit(Literal::Var(s.to_owned()))
     }
 
+    fn make_builtin(s: &str, arity: usize) -> Expr {
+        Expr::Builtin(s.to_owned(), arity, vec![])
+    }
+
     fn make_int(n: i64) -> Expr {
         Expr::Lit(Literal::Int(n))
     }
@@ -57,10 +64,28 @@ impl Expr {
         Expr::App(Box::new(f), Box::new(x))
     }
 
+    fn make_apps(f: Expr, xs: Vec<Expr>) -> Expr {
+        let mut ret = f;
+        for x in xs {
+            ret = Expr::make_app(ret, x);
+        }
+        ret
+    }
+
     fn var(&self) -> Option<&str> {
         match self {
             Expr::Lit(lit) => match lit {
                 Literal::Var(v) => Some(&v),
+                _ => None,
+            },
+            _ => None,
+        }
+    }
+
+    fn int(&self) -> Option<i64> {
+        match self {
+            Expr::Lit(lit) => match lit {
+                Literal::Int(n) => Some(*n),
                 _ => None,
             },
             _ => None,
@@ -94,6 +119,7 @@ impl Expr {
             Expr::Lam(var, x) => format!("\\{} -> {}", var, x.print()),
             Expr::App(f, x) => format!("({} {})", f.print(), x.print()),
             Expr::Lit(lit) => lit.print(),
+            Expr::Builtin(f, _, xs) => format!("<builtin:{}:{}>", f, xs.len()),
         }
     }
 
@@ -104,7 +130,9 @@ impl Expr {
             Expr::Lit(lit) => match lit {
                 Literal::Int(n) => format!("{}", n),
                 Literal::Var(v) => format!("({})", v),
+                _ => unreachable!(),
             },
+            _ => unreachable!(),
         }
     }
 
@@ -120,17 +148,40 @@ impl Expr {
                 } else {
                     match x.as_ref() {
                         Expr::Lam(_, _) => Expr::Lam(var.to_string(), Box::new(x.cexpr())).cexpr(),
-                        Expr::App(f, x) => Expr::make_app(
-                            Expr::make_app(
-                                Expr::make_var("s"),
-                                Expr::Lam(var.to_string(), f.clone()).cexpr(),
-                            ),
-                            Expr::Lam(var.to_string(), x.clone()).cexpr(),
-                        ),
+                        Expr::App(f, x) => {
+                            let fc = f.contains(var);
+                            let xc = x.contains(var);
+
+                            if fc && xc {
+                                Expr::make_app(
+                                    Expr::make_app(
+                                        Expr::make_var("s"),
+                                        Expr::Lam(var.to_string(), f.clone()).cexpr(),
+                                    ),
+                                    Expr::Lam(var.to_string(), x.clone()).cexpr(),
+                                )
+                            } else if fc {
+                                Expr::make_app(
+                                    Expr::make_app(
+                                        Expr::make_var("c"),
+                                        Expr::Lam(var.to_string(), f.clone()).cexpr(),
+                                    ),
+                                    x.cexpr(),
+                                )
+                            } else if xc {
+                                Expr::make_app(
+                                    Expr::make_app(Expr::make_var("b"), f.cexpr()),
+                                    Expr::Lam(var.to_string(), x.clone()).cexpr(),
+                                )
+                            } else {
+                                unreachable!()
+                            }
+                        }
                         _ => unreachable!(),
                     }
                 }
             }
+            _ => unreachable!(),
         }
     }
 
@@ -148,7 +199,164 @@ impl Expr {
                     x.contains(var)
                 }
             }
+            _ => unreachable!(),
         }
+    }
+
+    fn eval(&self, dict: &BTreeMap<String, Expr>) -> Expr {
+        // eprintln!("{:?}", self.print());
+        match self {
+            Expr::Lam(_, _) => unreachable!(),
+            Expr::App(f, x) => {
+                let f = f.eval(dict);
+                match f {
+                    Expr::Builtin(f, arity, mut xs) => {
+                        xs.push(x.as_ref().clone());
+                        if xs.len() == arity {
+                            eval_builtin(&f, &xs, dict)
+                        } else {
+                            Expr::Builtin(f, arity, xs)
+                        }
+                    }
+                    _ => unreachable!("Invalid app: {:?} {:?}", f.print(), x.print()),
+                }
+            }
+            Expr::Builtin(_, _, _) => self.clone(),
+            Expr::Lit(lit) => match lit {
+                Literal::Int(_n) => self.clone(),
+                Literal::Var(var) => {
+                    if let Some(e) = dict.get(var.as_str()) {
+                        e.eval(dict)
+                    } else {
+                        panic!("variable: {} is not defined", var)
+                    }
+                }
+                Literal::Canvas(_) => self.clone(),
+            },
+        }
+    }
+
+    fn to_value(&self, dict: &BTreeMap<String, Expr>) -> Value {
+        if let Some(n) = self.int() {
+            Value::int(n)
+        } else {
+            match self {
+                Expr::Lit(Literal::Canvas(ps)) => Value::Canvas(ps.clone()),
+                _ => {
+                    let b = Expr::make_apps(
+                        Expr::make_var("isnil"),
+                        vec![self.clone(), Expr::make_int(1), Expr::make_int(0)],
+                    )
+                    .eval(dict);
+
+                    let b = b.int().unwrap();
+
+                    if b == 1 {
+                        Value::Nil
+                    } else {
+                        let car = Expr::make_app(Expr::make_var("car"), self.clone()).eval(dict);
+                        let cdr = Expr::make_app(Expr::make_var("cdr"), self.clone()).eval(dict);
+                        Value::cons(car.to_value(dict), cdr.to_value(dict))
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn eval_builtin(f: &str, xs: &[Expr], dict: &BTreeMap<String, Expr>) -> Expr {
+    match f {
+        "s" => {
+            let x0 = xs[0].clone();
+            let x1 = xs[1].clone();
+            let x2 = xs[2].clone();
+            Expr::make_app(Expr::make_app(x0, x2.clone()), Expr::make_app(x1, x2)).eval(dict)
+        }
+        "k" => xs[0].eval(dict),
+        "i" => xs[0].eval(dict),
+
+        "b" => {
+            let x0 = xs[0].clone();
+            let x1 = xs[1].clone();
+            let x2 = xs[2].clone();
+            Expr::make_app(x0, Expr::make_app(x1, x2)).eval(dict)
+        }
+        "c" => {
+            let x0 = xs[0].clone();
+            let x1 = xs[1].clone();
+            let x2 = xs[2].clone();
+            Expr::make_app(Expr::make_app(x0, x2), x1).eval(dict)
+        }
+
+        "add" => {
+            let x = xs[0].eval(dict).int().unwrap();
+            let y = xs[1].eval(dict).int().unwrap();
+            Expr::make_int(x + y)
+        }
+        "mul" => {
+            let x = xs[0].eval(dict).int().unwrap();
+            let y = xs[1].eval(dict).int().unwrap();
+            Expr::make_int(x * y)
+        }
+        "div" => {
+            let x = xs[0].eval(dict).int().unwrap();
+            let y = xs[1].eval(dict).int().unwrap();
+            Expr::make_int(x / y)
+        }
+        "neg" => {
+            let x = xs[0].eval(dict).int().unwrap();
+            Expr::make_int(-x)
+        }
+        "eq" => {
+            let x = xs[0].eval(dict).int().unwrap();
+            let y = xs[1].eval(dict).int().unwrap();
+            dict.get(if x == y { "t" } else { "f" }).unwrap().eval(dict)
+        }
+        "lt" => {
+            let x = xs[0].eval(dict).int().unwrap();
+            let y = xs[1].eval(dict).int().unwrap();
+            dict.get(if x < y { "t" } else { "f" }).unwrap().eval(dict)
+        }
+
+        "ifzero" => {
+            let b = xs[0].eval(dict).int().unwrap();
+            if b == 0 {
+                xs[1].eval(dict)
+            } else {
+                xs[2].eval(dict)
+            }
+        }
+
+        "draw" => {
+            let xs = xs[0].eval(dict);
+            let mut xs = xs.to_value(dict);
+            let mut ps = vec![];
+
+            loop {
+                match xs {
+                    Value::Nil => {
+                        break;
+                    }
+                    Value::Cons(hd, tail) => match *hd {
+                        Value::Cons(x, y) => match (*x, *y) {
+                            (Value::Int(x), Value::Int(y)) => {
+                                ps.push((x, y));
+                                xs = *tail;
+                            }
+                            _ => unreachable!(),
+                        },
+                        _ => unreachable!(),
+                    },
+                    _ => unreachable!(),
+                }
+            }
+
+            Expr::Lit(Literal::Canvas(ps))
+        }
+
+        "send" => todo!(),
+
+        _ => unreachable!(),
     }
 }
 
@@ -184,6 +392,7 @@ enum Value {
     Int(i64),
     Nil,
     Cons(Box<Value>, Box<Value>),
+    Canvas(Vec<(i64, i64)>),
 }
 
 impl Value {
@@ -242,6 +451,7 @@ impl Value {
                 hd.modulate(v);
                 tl.modulate(v);
             }
+            _ => unreachable!(),
         }
     }
 
@@ -272,9 +482,21 @@ impl Value {
 
     fn print(&self) -> String {
         match self {
-            &Value::Int(n) => format!("{}", n),
+            Value::Int(n) => format!("{}", *n),
             Value::Nil => "nil".to_string(),
-            Value::Cons(hd, tl) => format!("({}, {})", hd.print(), tl.print()),
+            Value::Cons(hd, tl) => format!("({} . {})", hd.print(), tl.print()),
+            Value::Canvas(ps) => format!("<canvas:{:?}>", ps),
+        }
+    }
+
+    fn to_expr(&self) -> Expr {
+        match self {
+            Value::Int(n) => Expr::make_int(*n),
+            Value::Nil => Expr::make_var("nil"),
+            Value::Cons(hd, tl) => {
+                Expr::make_apps(Expr::make_var("cons"), vec![hd.to_expr(), tl.to_expr()])
+            }
+            Value::Canvas(ps) => Expr::Lit(Literal::Canvas(ps.clone())),
         }
     }
 
@@ -283,6 +505,7 @@ impl Value {
             &Value::Int(n) => format!("{}", n),
             Value::Nil => "()".to_string(),
             Value::Cons(hd, tl) => format!("({} . {})", hd.to_sexp(), tl.to_sexp()),
+            _ => unreachable!(),
         }
     }
 }
@@ -501,7 +724,28 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             println!("{}", v.to_sexp());
         }
         Opt::Parse(_opt) => {
-            let mut defs = vec![];
+            let mut dict = BTreeMap::new();
+
+            let builtins = [
+                ("s", 3),
+                ("k", 2),
+                ("i", 1),
+                ("b", 3),
+                ("c", 3),
+                ("add", 2),
+                ("mul", 2),
+                ("div", 2),
+                ("neg", 1),
+                ("eq", 2),
+                ("lt", 2),
+                ("ifzero", 3),
+                ("draw", 1),
+                ("send", 1),
+            ];
+
+            for (name, arity) in builtins.iter() {
+                dict.insert(name.to_string(), Expr::make_builtin(name, *arity));
+            }
 
             loop {
                 let mut s = String::new();
@@ -510,6 +754,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     break;
                 }
                 if s.chars().all(|c| c.is_whitespace()) {
+                    continue;
+                }
+                if s.starts_with("//") {
                     continue;
                 }
 
@@ -532,7 +779,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
                     dbg!(&rhs.print());
 
-                    defs.push((lhs.var().unwrap().to_owned(), rhs.cexpr()));
+                    dict.insert(lhs.var().unwrap().to_string(), rhs.cexpr());
                 } else {
                     // println!("{}", parse_expr(&s).refine().pp());
                     panic!()
@@ -541,20 +788,30 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
             // println!(r#"(load "lib.scm")"#);
 
-            for (var, expr) in defs {
+            for (var, expr) in dict.iter() {
                 // println!("(define ({}) {})", var, expr.print());
                 println!("{} = {}", var, expr.print());
             }
+
+            let state = Expr::make_var("nil");
+
+            let state = parse_sexp_str("(0 . ((0 . ()) . (0 . (() . ()))))")
+                .unwrap()
+                .to_expr();
+
             let expr = Expr::make_app(
                 Expr::make_app(
                     Expr::make_app(Expr::make_var("interact"), Expr::make_var("galaxy")),
-                    Expr::make_var("nil"),
+                    state,
                 ),
                 Expr::make_app(
                     Expr::make_app(Expr::make_var("vec"), Expr::make_int(0)),
                     Expr::make_int(0),
                 ),
             );
+
+            let t = expr.eval(&dict);
+            eprintln!("{}", t.to_value(&dict).print());
         }
     }
 
