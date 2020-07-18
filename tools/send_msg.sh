c7759fce2265b6735bf67105027e4432fa560092
chiro <i@chir.jp>
2020-07-18 10:42:40 +0900

    send_msg: Show demodulated response.

--- tools/send_msg.sh
+++ tools/send_msg.sh
@@ -5,4 +5,4 @@ cd $(dirname $0)
 mod="$(echo "$1" | ./modulate.sh || exit 1)"
 
 echo -n "$1 => " 1>&2
-curl -s -X POST "https://icfpc2020-api.testkontur.ru/aliens/send?apiKey=REDACTED" -H "accept: */*" -H "Content-Type: text/plain" -d "$mod"
+curl -s -X POST "https://icfpc2020-api.testkontur.ru/aliens/send?apiKey=REDACTED" -H "accept: */*" -H "Content-Type: text/plain" -d "$mod" | ./demodulate.sh
