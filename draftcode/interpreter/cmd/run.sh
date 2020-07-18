4050950c787c7c2d736d7c58a22eb241c8c37875
Masaya Suzuki <masayasuzuki@google.com>
2020-07-17 17:21:42 -0700

    draftcode: Separate the layers

--- draftcode/interpreter/cmd/run.sh
+++ draftcode/interpreter/cmd/run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-go run main.go ../../../messages/galaxy.txt "ap ap galaxy   ap ap cons $1         ap ap cons      ap ap cons $2 nil           ap ap cons 0     ap ap cons nil nil                   ap ap cons $3 $4" | gosh
+# go run main.go ../../../messages/galaxy.txt "ap ap galaxy   ap ap cons $1         ap ap cons      ap ap cons $2 nil           ap ap cons 0     ap ap cons nil nil                   ap ap cons $3 $4" | gosh
+go run main.go ../../../messages/galaxy.txt \
+  "ap ap galaxy   ap ap cons $1         ap ap cons    ap ap cons $2   ap ap cons $3 nil           ap ap cons 0     ap ap cons nil nil                   ap ap cons $4 $5" | gosh
