6c77b5a1d256aac2ea79f06badaee192e88d8e96
Masaya Suzuki <masayasuzuki@google.com>
2020-07-18 09:20:36 -0700

    examples: Use interact.py to interact with the servers

--- examples/app.cc
+++ examples/app.cc
@@ -1,24 +1,13 @@
-#include <cpr/cpr.h>
-
 #include <iostream>
 #include <string>
 
 int main(int argc, char* argv[]) {
-  const std::string server_url{argv[1]};
-  const std::string player_key{argv[2]};
+  const std::string player_key{argv[1]};
 
-  std::cout << "ServerUrl: " << server_url << "; PlayerKey: " << player_key
-            << std::endl;
+  std::cout << player_key << std::endl;
+  std::string response;
+  std::getline(std::cin, response);
 
-  auto res =
-      cpr::Post(cpr::Url{server_url}, cpr::Body{"(2, " + player_key + ", ())"},
-                cpr::Header{{"Content-Type", "text/plain"}});
-  if (res.status_code != 200) {
-    std::cout << "Unexpected server response:\nHTTP code: " << res.status_code
-              << "\nResponse body: " << res.text << std::endl;
-    return 2;
-  }
-
-  std::cout << "Server response: " << res.text << std::endl;
+  std::cerr << "Server response: " << response << std::endl;
   return 0;
 }
--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-bazel-bin/examples/app "$@" || echo "run error code: $?"
+MAIN_PROGRAM=bazel-bin/examples/app
+
+exec python3 ./infra/interact/interact.py "$1" "$2" "$MAIN_PROGRAM" --log=INFO
