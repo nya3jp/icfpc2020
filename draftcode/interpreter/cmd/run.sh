e0b03fc774c4c93ca3a1c75bc63bc6c50e7ea1bd
Masaya Suzuki <masayasuzuki@google.com>
2020-07-17 17:05:08 -0700

    Add more comments

---
+++ draftcode/interpreter/cmd/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+go run main.go ../../../messages/galaxy.txt "ap ap galaxy   ap ap cons $1         ap ap cons      ap ap cons $2 nil           ap ap cons 0     ap ap cons nil nil                   ap ap cons $3 $4" | gosh
