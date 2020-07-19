615556a7b623e5bcfd31e6d453263083ac1d276c
Masaya Suzuki <masayasuzuki@google.com>
2020-07-19 08:03:55 -0700

    infra/tester: Add options for logging

--- draftcode/api_test/src/main.rs
+++ draftcode/api_test/src/main.rs
@@ -2,5 +2,10 @@ extern crate rust_game_base;
 
 fn main() {
     rust_game_base::send_join_request();
-    rust_game_base::send_start_request(4, 4, 4, 4);
+    rust_game_base::send_start_request(&rust_game_base::Param {
+        energy: 4,
+        laser_power: 4,
+        cool_down_per_turn: 4,
+        life: 4,
+    });
 }
