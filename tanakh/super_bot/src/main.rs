14eca347659548e769a355559f1dbd1490af00e8
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2020-07-20 08:56:48 +0900

    super improved bot

--- tanakh/super_bot/src/main.rs
+++ tanakh/super_bot/src/main.rs
@@ -2,12 +2,17 @@ use anyhow::{anyhow, bail, Context, Result};
 use rust_game_base::*;
 use std::cmp::{max, min};
 
+mod sa;
+
+use sa::*;
+
 // 知見
 // レーザーの威力はmin(初期設定のlaser_power, 発射時のパラメーター)
 // heatは実際に発射された威力分上昇
 
 const MAX_STEP: usize = 256;
 
+#[derive(Clone)]
 struct Bot {
     stage: CurrentGameState,
     static_info: StageData,
@@ -25,6 +30,104 @@ static VECT: &[Point] = &[
     Point::new(1, 1),
     Point::new(0, 0),
 ];
+
+#[derive(Clone)]
+struct Problem {
+    bot: Bot,
+}
+
+impl Annealer for Problem {
+    type State = Vec<(i64, i64)>;
+    type Move = (usize, (i64, i64), (i64, i64));
+
+    fn init_state(&self, rng: &mut impl rand::Rng) -> Self::State {
+        let rest_step = self.bot.static_info.total_turns - self.bot.state.turn;
+        let mut v = vec![(0, 0); rest_step];
+
+        for r in v.iter_mut() {
+            r.0 = rng.gen_range(-1, 2);
+            r.1 = rng.gen_range(-1, 2);
+        }
+
+        v
+    }
+
+    fn eval(&self, state: &Self::State) -> f64 {
+        let me = self.bot.get_me();
+
+        let mut cur = me.position;
+        let mut heat = me.heat;
+        let mut velo = me.velocity;
+
+        let mut ovh_pena = 0;
+        let mut comp_step = 0;
+        let mut use_acc = 0;
+
+        for &(dx, dy) in state.iter() {
+            velo -= Point::new(dx as _, dy as _);
+            velo += get_gravity(&self.bot.state, &cur);
+            cur += velo;
+
+            if !self.bot.is_safe(&cur) {
+                break;
+            }
+
+            if dx != 0 || dy != 0 {
+                heat += 8;
+                use_acc += 1;
+            }
+
+            heat -= min(heat, me.params.cool_down_per_turn);
+
+            if heat > 64 {
+                let over = heat - 64;
+                ovh_pena += over;
+                heat = 64;
+            }
+
+            comp_step += 1;
+        }
+
+        // dbg!(comp_step, state.len(), use_acc, ovh_pena);
+
+        let score = if comp_step < state.len() {
+            // 完走できず
+            1000 + state.len() - comp_step
+        } else {
+            // アクセル使った回数にオーバーヒートペナを加味して
+            use_acc + ovh_pena * 10
+        };
+
+        score as f64
+    }
+
+    fn neighbour(
+        &self,
+        state: &Self::State,
+        rng: &mut impl rand::Rng,
+        _progress: f64,
+    ) -> Self::Move {
+        loop {
+            let ix = rng.gen_range(0, state.len());
+            let dx = rng.gen_range(-1, 2);
+            let dy = rng.gen_range(-1, 2);
+
+            if (dx, dy) == state[ix] {
+                continue;
+            }
+
+            break (ix, (dx, dy), state[ix]);
+        }
+    }
+
+    fn apply(&self, state: &mut Self::State, mov: &Self::Move) {
+        state[mov.0] = mov.1;
+    }
+
+    fn unapply(&self, state: &mut Self::State, mov: &Self::Move) {
+        state[mov.0] = mov.2;
+    }
+}
 
 impl Bot {
     fn new() -> Result<Bot> {
@@ -67,25 +170,46 @@ impl Bot {
                 continue;
             }
 
-            if param_rest >= 12
-                && param.cool_down_per_turn * 12 <= param.energy
-                && param.cool_down_per_turn * 12 <= param.laser_power * 4
-            // && param.cool_down_per_turn * 12 <= param.life * 2
-            {
-                param.cool_down_per_turn += 1;
-                param_rest -= 12;
-                continue;
+            if self.static_info.self_role == Role::ATTACKER {
+                // アタッカーはまんべんなく振る
+                // cool_down_per_turn はこっちも8で十分か？
+                // TODO: もっといいパラメーターあるかも
+
+                if param_rest >= 12
+                    && param.cool_down_per_turn < 8
+                    && param.cool_down_per_turn * 12 <= param.energy
+                    && param.cool_down_per_turn * 12 <= param.laser_power * 4
+                {
+                    param.cool_down_per_turn += 1;
+                    param_rest -= 12;
+                    continue;
+                }
+
+                if param_rest >= 4
+                    && param.laser_power < 64
+                    && param.laser_power * 2 <= param.energy
+                    && (param.cool_down_per_turn >= 8
+                        || param.laser_power * 2 <= param.cool_down_per_turn * 12)
+                {
+                    param.laser_power += 1;
+                    param_rest -= 4;
+                    continue;
+                }
+            } else {
+                // ディフェンダーはパワーに振らない
+                // cool_down_per_turnは8あれば十分
+
+                if param_rest >= 12
+                    && param.cool_down_per_turn < 8
+                    && param.cool_down_per_turn * 12 <= param.energy
+                {
+                    param.cool_down_per_turn += 1;
+                    param_rest -= 12;
+                    continue;
+                }
             }
 
-            if param_rest >= 4
-                && param.laser_power * 4 <= param.energy
-                // && param.laser_power * 4 <= param.life * 2
-                && param.laser_power * 4 <= param.cool_down_per_turn * 12
-            {
-                param.laser_power += 1;
-                param_rest -= 4;
-                continue;
-            }
+            // ライフに振る必要ある？
 
             // if param_rest >= 2
             //     && param.life * 2 <= param.energy
@@ -129,53 +253,113 @@ impl Bot {
         // * 位置関係がいい感じならビームを打つ
         // * 分裂をどうするか
 
-        for m in self.state.machines.iter() {
-            let m = m.0;
-            if m.role != self.static_info.self_role {
-                continue;
+        // TODO: アタッカー：偶然自爆で殺せる位置にいたとき、自爆する
+        // TODO: ディフェンダー：打たれそうになったら、移動して直撃を避けようとする
+
+        let sol = annealing(
+            &Problem { bot: self.clone() },
+            &AnnealingOptions::new(0.2, 1000.0, 0.1),
+        );
+
+        if !sol.is_empty() {
+            let dx = sol[0].0;
+            let dy = sol[0].1;
+
+            if dx != 0 || dy != 0 {
+                cmds.push(Command::Thrust(
+                    self.get_me().machine_id as _,
+                    Point::new(dx as _, dy as _),
+                ));
             }
+        }
 
-            // 一番長く生き残るところに行く
-            let (best_time, _, best_v) = VECT
-                .iter()
-                .map(|v| (self.live_time(&m, v), -(v.x.abs() + v.y.abs()), v))
-                .max_by_key(|r| r.0)
-                .unwrap();
+        if cmds.is_empty() {
+            if self.get_me().role == Role::ATTACKER {
+                // いい感じのポジショニングならビームを打つ
 
-            if best_v.x == 0 && best_v.y == 0 {
-                continue;
+                let ene = self.get_some_enemy();
+                let me = self.get_me();
+
+                let heat_mergin = 64 - me.heat + me.params.cool_down_per_turn;
+
+                // 次の位置予測
+                let next_ene_pos =
+                    ene.position + ene.velocity + get_gravity(&self.state, &ene.position);
+
+                let next_me_pos =
+                    me.position + me.velocity + get_gravity(&self.state, &me.position);
+
+                let mut best_dmg = (0, 0);
+                let mut cand = None;
+
+                for dy in -1..=1 {
+                    for dx in -1..=1 {
+                        // ここにはいないだろう
+                        if !self.is_safe(&next_ene_pos) {
+                            continue;
+                        }
+
+                        let v = next_me_pos - Point::new(dx, dy) - next_ene_pos;
+
+                        // スイートスポット以外は打たない
+                        if !(v.x == 0 || v.y == 0 || v.x.abs() == v.y.abs()) {
+                            continue;
+                        }
+
+                        // ずれてるときは移動しながら打つのでヒートに余裕を見る
+                        let max_beam_pow = min(
+                            heat_mergin as isize - if dx == 0 && dy == 0 { 0 } else { 8 },
+                            me.params.laser_power as isize,
+                        );
+
+                        let decay = max(v.x.abs(), v.y.abs());
+                        let dmg = max_beam_pow * 3 - decay;
+
+                        let dd = -(dx.abs() + dy.abs());
+
+                        if (dmg, dd) <= best_dmg {
+                            continue;
+                        }
+
+                        best_dmg = (dmg, dd);
+                        cand = Some((dx, dy, max_beam_pow));
+                    }
+                }
+
+                if let Some((dx, dy, beam_pow)) = cand {
+                    if dx != 0 || dy != 0 {
+                        cmds.push(Command::Thrust(me.machine_id, Point::new(dx, dy)));
+                    }
+                    cmds.push(Command::Beam(me.machine_id, next_ene_pos, beam_pow as _));
+                }
             }
-
-            dbg!(best_time, best_v);
-
-            cmds.push(Command::Thrust(m.machine_id as _, *best_v));
         }
 
         cmds
     }
 
-    fn live_time(&self, r: &Machine, acc: &Point) -> usize {
-        let step_limit = MAX_STEP - self.state.turn;
+    // fn live_time(&self, r: &Machine, acc: &Point) -> usize {
+    //     let step_limit = MAX_STEP - self.state.turn;
 
-        let mut ret = 0;
-        let mut v = r.velocity - *acc;
-        let mut cur = r.position;
+    //     let mut ret = 0;
+    //     let mut v = r.velocity - *acc;
+    //     let mut cur = r.position;
 
-        while ret < step_limit {
-            v += get_gravity(&self.state, &cur);
-            cur += v;
+    //     while ret < step_limit {
+    //         v += get_gravity(&self.state, &cur);
+    //         cur += v;
 
-            if !self.is_safe(&cur) {
-                break;
-            }
+    //         if !self.is_safe(&cur) {
+    //             break;
+    //         }
 
-            ret += 1;
-        }
+    //         ret += 1;
+    //     }
 
-        dbg!(ret);
+    //     dbg!(ret);
 
-        ret
-    }
+    //     ret
+    // }
 
     fn is_safe(&self, p: &Point) -> bool {
         if let Some(obs) = &self.static_info.obstacle {
@@ -190,6 +374,26 @@ impl Bot {
             }
         }
         true
+    }
+
+    fn get_me(&self) -> &Machine {
+        for m in self.state.machines.iter() {
+            let m = &m.0;
+            if m.role == self.static_info.self_role {
+                return m;
+            }
+        }
+        panic!("Cannot find me")
+    }
+
+    fn get_some_enemy(&self) -> &Machine {
+        for m in self.state.machines.iter() {
+            let m = &m.0;
+            if m.role != self.static_info.self_role {
+                return m;
+            }
+        }
+        panic!("Cannot find enemy")
     }
 }
 
---
+++ tanakh/super_bot/src/sa.rs
@@ -0,0 +1,156 @@
+use rand::prelude::*;
+use std::thread;
+use std::time::SystemTime;
+
+#[derive(Clone)]
+pub struct AnnealingOptions {
+    pub time_limit: f64,
+    pub max_temp: f64,
+    pub min_temp: f64,
+    pub seed: u64,
+    pub restart: usize,
+    pub threads: usize,
+    pub silent: bool,
+}
+
+impl AnnealingOptions {
+    pub fn new(time_limit: f64, max_temp: f64, min_temp: f64) -> Self {
+        Self {
+            time_limit,
+            max_temp,
+            min_temp,
+            seed: 777,
+            restart: 1,
+            threads: 1,
+            silent: false,
+        }
+    }
+}
+
+pub trait Annealer {
+    type State: Clone + Send + Sync;
+    type Move;
+
+    fn init_state(&self, rng: &mut impl Rng) -> Self::State;
+
+    fn eval(&self, state: &Self::State) -> f64;
+
+    fn neighbour(&self, state: &Self::State, rng: &mut impl Rng, progress: f64) -> Self::Move;
+
+    fn apply(&self, state: &mut Self::State, mov: &Self::Move);
+    fn unapply(&self, state: &mut Self::State, mov: &Self::Move);
+
+    fn apply_and_eval(&self, state: &mut Self::State, mov: &Self::Move, _prev_score: f64) -> f64 {
+        self.apply(state, mov);
+        self.eval(state)
+    }
+}
+
+pub fn annealing<A: 'static + Annealer + Clone + Send>(
+    annealer: &A,
+    opt: &AnnealingOptions,
+) -> <A as Annealer>::State {
+    assert!(opt.threads > 0);
+
+    if opt.threads == 1 {
+        do_annealing(None, annealer, opt).1
+    } else {
+        let mut ths = vec![];
+        let mut rng = StdRng::seed_from_u64(opt.seed);
+
+        for i in 0..opt.threads {
+            let a = annealer.clone();
+            let mut o = opt.clone();
+            o.seed = rng.gen();
+            ths.push(thread::spawn(move || do_annealing(Some(i), &a, &o)));
+        }
+
+        ths.into_iter()
+            .map(|th| th.join().unwrap())
+            .min_by(|a, b| a.0.partial_cmp(&b.0).unwrap())
+            .unwrap()
+            .1
+    }
+}
+
+fn do_annealing<A: Annealer>(
+    thread_id: Option<usize>,
+    annealer: &A,
+    opt: &AnnealingOptions,
+) -> (f64, <A as Annealer>::State) {
+    let mut rng = SmallRng::seed_from_u64(opt.seed);
+
+    let mut state = annealer.init_state(&mut rng);
+    let mut cur_score = annealer.eval(&state);
+    let mut best_score = cur_score;
+    let mut best_ans = state.clone();
+
+    macro_rules! progress {
+        ($($arg:expr),*) => {
+            if !opt.silent {
+                if let Some(tid) = thread_id {
+                    eprint!("[{:02}] ", tid);
+                }
+                eprintln!($($arg),*);
+            }
+        };
+    }
+
+    progress!("Initial score: {}", cur_score);
+
+    let t_max = opt.max_temp;
+    let t_min = opt.min_temp;
+
+    let now = SystemTime::now();
+    let x = (t_min / t_max).ln();
+
+    let mut progress = 0.0;
+    let mut temp = t_max;
+
+    for turn in 0.. {
+        if turn % 1000 == 0 {
+            let elapsed = now.elapsed().unwrap().as_secs_f64();
+            if elapsed >= opt.time_limit {
+                progress!("{} steps done.", fmt_number(turn));
+                break;
+            }
+
+            progress = elapsed / opt.time_limit;
+            temp = t_max * (x * progress).exp();
+        }
+
+        let mov = annealer.neighbour(&state, &mut rng, progress);
+        let new_score = annealer.apply_and_eval(&mut state, &mov, cur_score);
+
+        if new_score <= cur_score || rng.gen_bool(((cur_score - new_score) / temp).exp()) {
+            cur_score = new_score;
+            if cur_score < best_score {
+                if best_score - cur_score > 1e-6 {
+                    progress!("Best: score = {:.3}, temp = {:.9}", cur_score, temp);
+                }
+                best_score = cur_score;
+                best_ans = state.clone();
+            }
+        } else {
+            annealer.unapply(&mut state, &mov);
+        }
+    }
+
+    (best_score, best_ans)
+}
+
+fn fmt_number(n: usize) -> String {
+    if n < 1_000 {
+        format!("{}", n)
+    } else if n < 1_000_000 {
+        format!("{:.3}K", n as f64 / 1_000.0)
+    } else if n < 1_000_000_000 {
+        format!("{:.3}M", n as f64 / 1_000_000.0)
+    } else if n < 1_000_000_000_000 {
+        format!("{:.3}G", n as f64 / 1_000_000_000.0)
+    } else if n < 1_000_000_000_000_000 {
+        format!("{:.3}T", n as f64 / 1_000_000_000_000.0)
+    } else {
+        todo!()
+    }
+}
