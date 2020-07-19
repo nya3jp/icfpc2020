29354aeca590764286163e45e46621f9b94bbbba
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-07-20 02:33:49 +0900

    bot/simple_stay: copy from bot/kimiyuki

---
+++ bot/simple_stay/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+exec python3 ../../infra/interact/interact.py "$1" "$2" ./target/release/kimiyuki --log=INFO
---
+++ bot/simple_stay/src/main.rs
@@ -0,0 +1,35 @@
+#![allow(non_snake_case, unused, non_upper_case_globals)]
+
+extern crate rust_game_base;
+#[macro_use]
+extern crate itertools;
+
+use rust_game_base::Point;
+use rust_game_base::actions;
+
+fn main() {
+    let start_params = initialize();
+    let resp = rust_game_base::send_join_request();
+    let mut resp = rust_game_base::send_start_request(&start_params).unwrap();
+    while resp.current_game_state != rust_game_base::CurrentGameState::END {
+        let commands = play(&resp);
+        resp = rust_game_base::send_command_request(&mut commands.into_iter()).unwrap();
+    }
+}
+
+fn initialize() -> rust_game_base::Param {
+    let start_params = rust_game_base::Param { energy: 300, laser_power: 0, cool_down_per_turn: 8, life: 1 };
+    start_params
+}
+
+fn play(resp: &rust_game_base::Response) -> Vec<rust_game_base::Command> {
+    let mut commands = vec![];
+    let current_state = resp.current_state.as_ref().unwrap();
+    for (machine, _) in current_state.machines.iter() {
+        match actions::stay(current_state, machine.machine_id) {
+            Some(command) => commands.push(command),
+            None => (),
+        }
+    }
+    commands
+}
