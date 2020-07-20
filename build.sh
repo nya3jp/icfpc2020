17f0444ff6b3519bc3b90e668b94ac5ebd57f882
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2020-07-20 18:24:17 +0900

    Removed unused files

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# bazel --batch build --distdir=/bazel/dist -c opt //examples:app
-
-cd ./draftcode/api_test/
-cargo build --release --offline
-
--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# MAIN_PROGRAM=bazel-bin/examples/app
-MAIN_PROGRAM=draftcode/api_test/target/release/api_test
-
-exec python3 ./infra/interact/interact.py "$1" "$2" "$MAIN_PROGRAM" --log=INFO
